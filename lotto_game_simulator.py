b60f7ab2e69f5d52c5ee5e9751c18576f47a51a2
Karolina <karolina.leszczynska05@gmail.com>
2022-05-13 12:21:07 +0200

    Adding main.py and refactoring in the code

--- lotto_game_simulator.py
+++ lotto_game_simulator.py
@@ -1,10 +1,26 @@
 from random import randint
+from time import sleep
 
 
 class Lotto:
-    def __init__(self, user_numbers: list):
+    def __init__(self):
         self.winning_numbers = []
-        self.user_numbers = user_numbers
+        self.user_numbers = []
+        self.user_lucky_numbers = 0
+        self.win_rate = {6: "I", 5: "II", 4: "III", 3: "IV"}
+
+    def lucky_dip(self):
+        while len(self.user_numbers) < 6:
+            number = randint(1, 49)
+            if number not in self.user_numbers:
+                self.user_numbers.append(number)
+
+    def take_numbers_from_user(self):
+        while len(self.user_numbers) < 6:
+            number = int(input("Type a number from 1 to 49: "))
+            if number not in self.user_numbers:
+                self.user_numbers.append(number)
+        print(f"Your numbers are: {self.user_numbers}")
 
     def draw_numbers(self):
         while len(self.winning_numbers) < 6:
@@ -12,20 +28,28 @@ class Lotto:
             if number not in self.winning_numbers:
                 self.winning_numbers.append(number)
 
+        for number in self.winning_numbers:
+            sleep(2)
+            print(number)
+
     def check_numbers(self):
-        if len(set(self.winning_numbers) & set(self.user_numbers)) == len(self.winning_numbers):
-            print("YOU WON!!!")
+        for number in self.user_numbers:
+            if number in self.winning_numbers:
+                self.user_lucky_numbers += 1
+        if self.user_lucky_numbers == 0:
+            print("No match this time")
+        elif self.user_lucky_numbers == 6:
+            print("Congratulations. The main award is yours!")
         else:
-            print("Not this time")
+            print(f"You have successfully drawn only {self.user_lucky_numbers} numbers.")
+            return self.user_lucky_numbers
+
+    def win_rate_info(self):
+        if self.user_lucky_numbers >= 3:
+            print(f"Win rate is: {self.win_rate[self.user_lucky_numbers]}.")
+        else:
+            print("No award this time.")
 
     def __str__(self):
         return str(f"Your numbers: {self.user_numbers}\nLucky numbers: {self.winning_numbers}")
 
-
-if __name__ == "__main__":
-    my_numbers = [11, 22, 4, 6, 2, 14]
-
-    l1 = Lotto(my_numbers)
-    l1.draw_numbers()
-    l1.check_numbers()
-    print(l1)
---
+++ main.py
@@ -0,0 +1,40 @@
+from lotto_game_simulator import Lotto
+from time import sleep
+
+
+class Main:
+    print('''Welcome to Lotto!
+Lotto is game of chance where you or a machine select numbers
+and you win a prize if the numbers are picked in a random drawing.
+
+Take your chances with a Lucky Dip to play randomly selected numbers
+or click to enter your own 6 numbers.
+''')
+    draw = Lotto()
+
+    def take_decision(self):
+        print("Do you want to play randomly selected numbers? y/n? ")
+        decision = input()
+
+        if decision.lower() == "y":
+            self.draw.lucky_dip()
+
+        else:
+            self.draw.take_numbers_from_user()
+
+        print('''There are 49 sequentially placed balls in the cassette of the draw machine.
+The drum of a drawing machine is empty...
+the lock is released...
+the drawing begins...
+The drawn numbers are:''')
+
+        sleep(5)
+        self.draw.draw_numbers()
+        self.draw.check_numbers()
+        self.draw.win_rate_info()
+        print(self.draw)
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.take_decision()
