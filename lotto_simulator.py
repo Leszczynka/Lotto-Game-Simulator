f3584e91755ef36340abde909a038f5036f903ef
Karolina <karolina.leszczynska05@gmail.com>
2022-08-22 12:19:07 +0200

    Changing the structure of the project, create new file lotto_simulator.py

---
+++ lotto_simulator.py
@@ -0,0 +1,22 @@
+def draw_numbers():
+    pass
+
+
+def lucky_dip():
+    pass
+
+
+def user_interface():
+    pass
+
+
+def check_result():
+    pass
+
+
+def play():
+    pass
+
+
+def main():
+    pass
